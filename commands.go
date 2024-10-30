f6fb3e8bd756038726a8ecf84dd7bfa2cda294e5
samsamisamsam <samuel.larouche.cote@gmail.com>
2024-10-29 23:26:58 -0400

    mock catch command implemented

--- commands.go
+++ commands.go
@@ -9,29 +9,34 @@ type cliCommand struct {
 func getCommands() map[string]cliCommand {
 	return map[string]cliCommand{
 		"exit": {
-			name:        "exit",
+			name:        "  exit",
 			description: "Exit the Pokedex",
 			callback:    exitCallback,
 		},
 		"help": {
-			name:        "help",
+			name:        "  help",
 			description: "Displays available commands",
 			callback:    helpCallback,
 		},
 		"map": {
-			name:        "map",
+			name:        "   map",
 			description: " Displays a list of 20 locations",
 			callback:    mapCallback,
 		},
 		"mapb": {
-			name:        "mapb",
-			description: " Displays a list of the 20 previous locations",
+			name:        "  mapb",
+			description: "Displays a list of the 20 previous locations",
 			callback:    mapbCallback,
 		},
 		"explore": {
 			name:        "explore {location_area}",
-			description: " Displays wild pokemon who might be appearing in a location area",
+			description: "Displays wild pokemon who might be appearing in a location area",
 			callback:    exploreCallback,
+		},
+		"catch": {
+			name:        " catch {pokemon_name}",
+			description: "Try to catch a pokemon",
+			callback:    catchCallback,
 		},
 	}
 }
---
+++ internal/pokeapi/pokemon_req.go
@@ -0,0 +1,58 @@
+package pokeapi
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func (c *Client) GetPokemon(pokemonName string) (Pokemon, error) {
+	endpoint := "/pokemon/" + pokemonName
+	fullURL := baseURL + endpoint
+
+	// check the cache
+	data, ok := c.cache.Get(fullURL)
+	if ok {
+		// cache hit
+		fmt.Println("!!! cache hit !!!")
+		pokemon := Pokemon{}
+		err := json.Unmarshal(data, &pokemon)
+		if err != nil {
+			return Pokemon{}, nil
+		}
+		return pokemon, nil
+	}
+	fmt.Println("??? cache miss ???")
+
+	req, err := http.NewRequest("GET", fullURL, nil)
+	if err != nil {
+		return Pokemon{}, err
+	}
+
+	res, err := c.httpClient.Do(req)
+	if err != nil {
+		return Pokemon{}, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode > 399 {
+		return Pokemon{}, fmt.Errorf("status code error: %v", res.StatusCode)
+	}
+
+	data, err = io.ReadAll(res.Body)
+	if err != nil {
+		return Pokemon{}, err
+	}
+
+	pokemon := Pokemon{}
+	err = json.Unmarshal(data, &pokemon)
+	if err != nil {
+		return Pokemon{}, nil
+	}
+
+	c.cache.Add(fullURL, data)
+
+	return pokemon, nil
+
+}
